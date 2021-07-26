0fdb27dbfa699165caefaba7f1f468a35c8f7211
“jianglang” <lang.jiang@ximalaya.com>
2021-07-26 17:16:16 +0800

    feat: 提交 create <template-name> <project-name>功能代码

--- bin/megalo.js
+++ bin/megalo.js
@@ -3,11 +3,12 @@ const chalk = require('chalk');
 const semver = require('semver');
 const didyoumean = require('didyoumean');
 const requiredNodeVersion = require('../package.json').engines.node;
+const enhanceErrorMessages = require('../lib/utils/enhanceErrorMessages')
 
 didyoumean.threshold = 0.6;
 
 // 检测node版本是否符号要求范围
-function checkNodeVersion(wanted, cliName) {
+function checkNodeVersion (wanted, cliName) {
   if (!semver.satisfies(process.version, wanted)) {
     console.log(
       chalk.red(
@@ -26,6 +27,7 @@ function checkNodeVersion(wanted, cliName) {
 
 checkNodeVersion(requiredNodeVersion, 'megalo-cli');
 
+// 修改帮助信息的首行提示
 program
   .version(require('../package.json').version, '-v, --version')
   .usage('<command> [options]');
@@ -38,13 +40,57 @@ program
     // 输入参数校验
     validateArgsLen(process.argv.length, 5);
     require('../lib/template/easy-create')(
-      lowercase(templateName),
+      lowerCase(templateName),
       projectName
     );
   });
+
+// 添加一个项目模板
+program
+  .command('add <template-name> <git-repo-address>')
+  .description('add a project template')
+  .action()
+
+// 处理非法命令
+program.arguments('<command>').action(cmd => {
+  console.log(cmd)
+  program.outputHelp()
+  console.log(`  ` + chalk.red(`Unknown comman ${chalk.yellow(cmd)}.`))
+  console.log()
+  suggestCommands(cmd)
+})
+
+// 重写 commander 丢失参数事件 当参数缺失会触发Command.missingArgument事件
+enhanceErrorMessages('missingArgument', argsName => {
+  return `Missing required argument ${chalk.yellow(`<${argsName}>`)}`
+})
+
+// 把命令行参数提供给 commander 解析
+program.parse(process.argv)
+
+// 输入megalo显示帮助信息 ( 当不输入或者输入 --help 显示帮助新乡 )
+if (!process.argv.slice(2).length || process.argv.slice(2)[0] === '--help') {
+  program.outputHelp()
+}
 
 function validateArgsLen(argLen, MaxArgLength) {
   if (argLen > MaxArgLength) {
     console.log(chalk.yellow('\n 提示: 参数超过期望长度, 多余参数自动忽略'));
   }
+}
+
+
+// 对非法命令解析 给出相近的提示
+function suggestCommands (cmd) {
+  const avaliableCommands = program.commands.map(item => item._name)
+  // 简易只能匹配用户命令
+  const suggestion = didyoumean(cmd, avaliableCommands)
+
+  if (suggestion) {
+    console.log(' ' + chalk.red(`Did you mean ${chalk.yellow(suggestion)}?`))
+  }
+}
+
+function lowerCase(name) {
+  return name.toLocaleLowerCase()
 }
