bb28af9cd1f58b2715c7d9bc4013670e7a627b5c
“jianglang” <lang.jiang@ximalaya.com>
2021-07-26 19:38:37 +0800

    feat: 添加add delete list配置命令

--- bin/megalo.js
+++ bin/megalo.js
@@ -3,12 +3,12 @@ const chalk = require('chalk');
 const semver = require('semver');
 const didyoumean = require('didyoumean');
 const requiredNodeVersion = require('../package.json').engines.node;
-const enhanceErrorMessages = require('../lib/utils/enhanceErrorMessages')
+const enhanceErrorMessages = require('../lib/utils/enhanceErrorMessages');
 
 didyoumean.threshold = 0.6;
 
 // 检测node版本是否符号要求范围
-function checkNodeVersion (wanted, cliName) {
+function checkNodeVersion(wanted, cliName) {
   if (!semver.satisfies(process.version, wanted)) {
     console.log(
       chalk.red(
@@ -49,28 +49,50 @@ program
 program
   .command('add <template-name> <git-repo-address>')
   .description('add a project template')
-  .action()
+  .action((templateName, gitRepoAddress, cmd) => {
+    // 输入参数校验
+    validateArgsLen(process.argv.length, 5);
+    require('../lib/template/add-template')(templateName, gitRepoAddress);
+  });
+
+// 展示当前所有模板
+program
+  .command('list')
+  .description('list all available project template')
+  .action((cmd) => {
+    validateArgsLen(process.argv.length, 3);
+    require('../lib/template/list-template')();
+  });
+
+// 删除模板
+program
+  .command('delete <template-name>')
+  .description('delete a project template')
+  .action((templateName, cmd) => {
+    validateArgsLen(process.argv.length, 4);
+    require('../lib/template/delete-template')(templateName);
+  });
 
 // 处理非法命令
-program.arguments('<command>').action(cmd => {
-  console.log(cmd)
-  program.outputHelp()
-  console.log(`  ` + chalk.red(`Unknown comman ${chalk.yellow(cmd)}.`))
-  console.log()
-  suggestCommands(cmd)
-})
+program.arguments('<command>').action((cmd) => {
+  console.log(cmd);
+  program.outputHelp();
+  console.log(`  ` + chalk.red(`Unknown comman ${chalk.yellow(cmd)}.`));
+  console.log();
+  suggestCommands(cmd);
+});
 
 // 重写 commander 丢失参数事件 当参数缺失会触发Command.missingArgument事件
-enhanceErrorMessages('missingArgument', argsName => {
-  return `Missing required argument ${chalk.yellow(`<${argsName}>`)}`
-})
+enhanceErrorMessages('missingArgument', (argsName) => {
+  return `Missing required argument ${chalk.yellow(`<${argsName}>`)}`;
+});
 
 // 把命令行参数提供给 commander 解析
-program.parse(process.argv)
+program.parse(process.argv);
 
 // 输入megalo显示帮助信息 ( 当不输入或者输入 --help 显示帮助新乡 )
 if (!process.argv.slice(2).length || process.argv.slice(2)[0] === '--help') {
-  program.outputHelp()
+  program.outputHelp();
 }
 
 function validateArgsLen(argLen, MaxArgLength) {
@@ -79,18 +101,17 @@ function validateArgsLen(argLen, MaxArgLength) {
   }
 }
 
-
 // 对非法命令解析 给出相近的提示
-function suggestCommands (cmd) {
-  const avaliableCommands = program.commands.map(item => item._name)
+function suggestCommands(cmd) {
+  const avaliableCommands = program.commands.map((item) => item._name);
   // 简易只能匹配用户命令
-  const suggestion = didyoumean(cmd, avaliableCommands)
+  const suggestion = didyoumean(cmd, avaliableCommands);
 
   if (suggestion) {
-    console.log(' ' + chalk.red(`Did you mean ${chalk.yellow(suggestion)}?`))
+    console.log(' ' + chalk.red(`Did you mean ${chalk.yellow(suggestion)}?`));
   }
 }
 
 function lowerCase(name) {
-  return name.toLocaleLowerCase()
+  return name.toLocaleLowerCase();
 }
